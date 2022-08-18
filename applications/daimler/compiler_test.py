bfd2568d5d24621729e32da806915f4b8329d951
Florent Duchesne <florent.duchesne.1@ens.etsmtl.ca>
2022-08-18 10:05:56 -0400

    separable test

--- applications/daimler/compiler_test.py
+++ applications/daimler/compiler_test.py
@@ -8,6 +8,8 @@ class Tests(TestCase):
         generate("db", {"x": 18, "y": 36}, "img.db")
         train("img.db", "model.h5")
         compile("model.h5", ".", "img.db")
+        train("img.db", "model_separable.h5", use_separable=True)
+        compile("model_separable.h5", ".", "img.db")
 
 if __name__ == "__main__":
     t = Tests()
--- applications/daimler/train.py
+++ applications/daimler/train.py
@@ -12,45 +12,41 @@ if USE_PLAIDML:
         print("PlaidML not found, using Tensorflow")
         USE_PLAIDML = False
 import argparse
-from keras.layers import Flatten, MaxPooling2D, Convolution2D, Dropout, Dense
+from keras.layers import Flatten, MaxPooling2D, Convolution2D, SeparableConvolution2D, Dropout, Dense
 from keras.models import Sequential
 from applications.daimler.loader import load_imdb
 
-def train(imgdb_path, model_path):
+def create_conv_model(shape, ConvolutionType=Convolution2D):
+    conv_model = Sequential()
+    conv_model.add(ConvolutionType(8, (3, 3), input_shape=shape,
+                                activation='relu', padding='same'))
+    conv_model.add(MaxPooling2D(pool_size=(2, 2)))
+    conv_model.add(ConvolutionType(24, (3, 3), padding='same', activation='relu'))
+    conv_model.add(MaxPooling2D(pool_size=(2, 2)))
+    conv_model.add(ConvolutionType(48, (4, 3), padding='same', activation='relu'))
+    conv_model.add(MaxPooling2D(pool_size=(2, 2)))
+    conv_model.add(Dropout(0.4))
+    conv_model.add(Flatten())
+    conv_model.add(Dense(2, activation='softmax'))
+    return conv_model
+
+def train(imgdb_path, model_path, use_separable=False):
     imdb = load_imdb(imgdb_path)
     x = imdb['images']
     y = imdb['y']
 
-    usual_model = Sequential()
-    usual_model.add(Convolution2D(4, (3, 3), input_shape=(x.shape[1], x.shape[2], 1),
-                                activation='relu', padding='same'))
-    usual_model.add(MaxPooling2D(pool_size=(2, 2)))
-    usual_model.add(Convolution2D(16, (3, 3), padding='same', activation='relu'))
-    usual_model.add(MaxPooling2D(pool_size=(2, 2)))
-    usual_model.add(Convolution2D(32, (3, 3), padding='same', activation='relu',))
-    usual_model.add(MaxPooling2D(pool_size=(4, 2)))
-    usual_model.add(Dropout(0.4))
-    usual_model.add(Convolution2D(2, (2, 2), activation='softmax'))
-    usual_model.add(Flatten())
-
-    dense_model = Sequential()
-    dense_model.add(Convolution2D(4, (3, 3), input_shape=(x.shape[1], x.shape[2], 1),
-                                activation='relu', padding='same'))
-    dense_model.add(MaxPooling2D(pool_size=(2, 2)))
-    dense_model.add(Convolution2D(16, (3, 3), padding='same', activation='relu'))
-    dense_model.add(MaxPooling2D(pool_size=(2, 2)))
-    dense_model.add(Convolution2D(32, (3, 3), padding='same', activation='relu'))
-    dense_model.add(MaxPooling2D(pool_size=(2, 2)))
-    dense_model.add(Dropout(0.4))
-    dense_model.add(Flatten())
-    dense_model.add(Dense(2, activation='softmax'))
+    input_shape = (x.shape[1], x.shape[2], 1)
+    epochs = 30 if use_separable else 10
 
     # Select the current model here
-    model = dense_model
+    if use_separable:
+        model = create_conv_model(input_shape, SeparableConvolution2D)
+    else:
+        model = create_conv_model(input_shape, Convolution2D)
 
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     print(model.summary())
-    model.fit(x, y, batch_size=1000, epochs=10, verbose=1, validation_split=0.05)
+    model.fit(x, y, batch_size=1000, epochs=epochs, verbose=1, validation_split=0.05)
     model.save(model_path)
 
 if __name__ == "__main__":
@@ -61,11 +57,14 @@ if __name__ == "__main__":
                             'Default is img.db in current folder.')
     parser.add_argument('-m', '--model-path', dest='model_path',
                         help='Store the trained model using this path. Default is model.h5.')
+    parser.add_argument('-s', '--separable', dest='use_separable', action='store_true',
+                        help='Whether to use separable convolution instead of regular ones. Default is regular (Conv2D).')
 
     args = parser.parse_args()
 
     imgdb_path = "img.db"
     model_path = "model.h5"
+    use_separable = False
 
     if args.imgdb_path is not None:
         imgdb_path = args.imgdb_path
@@ -73,5 +72,8 @@ if __name__ == "__main__":
     if args.model_path is not None:
         model_path = args.model_path
     
-    train(imgdb_path, model_path)
+    if args.use_separable is not None:
+        use_separable = args.use_separable
+    
+    train(imgdb_path, model_path, use_separable)
 
